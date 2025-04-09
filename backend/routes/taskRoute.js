c94c495bbe3cf698c6141a92a9a49c6b46cc85ab
unknown <rahma.sbei@medtech.tn>
2025-04-09 10:28:26 +0100

    improvements

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -11,9 +11,10 @@ const {
 } = require("../controllers/taskController");
 
 //define endpoints
-taskRoute.get("/tasks", getTasks);
-taskRoute.get("/tasks/:id", getOneTask);
-taskRoute.post("/tasks", postTask);
-taskRoute.delete("/tasks/:id", deleteTask);
-taskRoute.patch("/tasks/:id", updateTaskStatus);
+taskRoute.get("/tasks", getTasks); //This endpoint returns a list of all existing tasks
+taskRoute.get("/tasks/:id", getOneTask); //This endpoint accepts id from URL params, returns details of a single task
+taskRoute.post("/tasks", postTask); //This endpoint accepts task object from request body, creates a new task
+taskRoute.delete("/tasks/:id", deleteTask); //This endpoint accepts id from URL params, deletes a task
+taskRoute.patch("/tasks/:id", updateTaskStatus); //This endpoint accepts id from URL params, updates the status of a task
+
 module.exports = taskRoute;
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -13,10 +13,15 @@ const {
 const { isAuth } = require("../middleware/isAuth");
 const { isAutho } = require("../middleware/isAutho");
 //define endpoints
-userRoute.get("/users", getUsers);
+// Define endpoints
+userRoute.get("/users", getUsers); //This endpoint returns a list of all users
+
+//This endpoint accepts id from URL params, checks if the signed-in user is authenticated and authorized, and returns details of a single user
 userRoute.get("/users/:id", isAuth, isAutho(["user", "admin"]), getOneUser);
-userRoute.post("/users", postUser);
-userRoute.put("/users/:id", putUser);
+userRoute.post("/users", postUser); //This endpoint accepts user data from request body, creates a new user
+userRoute.put("/users/:id", putUser); //This endpoint accepts id from URL params, updates an existing user's data
+
+//This endpoint accepts id from URL params, checks if the signed-in user is authenticated and authorized as an admin, and deletes a user
 userRoute.delete("/users/:id", isAuth, isAutho(["admin"]), deleteUser);
-userRoute.post("/signIn", signIn);
+userRoute.post("/signIn", signIn); //This endpoint accepts sign-in credentials from request body and returns a user authentication token
 module.exports = userRoute;
